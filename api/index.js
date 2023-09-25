380c7dd62637a7eee4bfbf603a1c06765ce99953
Mariano Vargas <marianoluisvargas@gmail.com>
2023-09-25 18:06:54 -0300

    deploy2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log("listening at " + port); // eslint-disable-line no-console
   });
 });
